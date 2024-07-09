7a2b1e118979a4ac554441f608eec35e39f61fc3
Adham Saeed <134134784+AdhamRuv@users.noreply.github.com>
2024-07-09 17:55:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// const viewPortWidth = window.innerWidth;
+// if(viewPortWidth < 635){
+//     console.log(viewPortWidth);
+//     const link = document.querySelector(".link");
+//     link.href = "styleMb.css";
+// };
